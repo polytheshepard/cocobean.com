d4c7e9ca316a7515ba421956c78593a4bfe0607a
Katies Truong <31566298+KatiesTruong@users.noreply.github.com>
2018-02-06 08:24:23 +1100

    Updated service2.php

--- service2.php
+++ service2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Login Page</title>
+    <title>Delivery</title>
     <link rel="stylesheet" type="text/css" href="service2-style.css">
     <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Montserrat:400,900|Signika" rel="stylesheet">    
 </head>
@@ -40,10 +40,55 @@
             <ul>
                 <div><li><a href="#lunch">Lunch</a></li></div>
                 <div><li><a href="#dinner">Dinner</a></li></div>
+                <div><li><a href="#drinks">Drinks</a></li></div>
             </ul>
         </div>
-        <h2 id="lunch">Lunch</h2>
-        <h2 id="dinner">Dinner</h2>
+        <div class="container">
+            <div class="lunch">
+            <h2 id="lunch">Lunch</h2>
+            <img class="img-bg" src="../img/lunch-background.jpeg" alt="Image of chips for lunch on table">
+            <div class="food-content">
+                <h2>Feta Cheese Salad</h2>
+                <img class="img-content" src="../img/feta-cheese-salad.jpeg" alt="">
+                <p>Price: $12</p>
+            </div>
+            <div class="food-content">
+                <h2>Chia Pudding</h2>
+                <img class="img-content" src="../img/chia-pudding.jpeg" alt="">
+                <p>Price: $13</p>
+            </div>
+            <div class="food-content">
+                <h2>Banana Crust</h2>
+                <img class="img-content" src="../img/banana-crust.jpeg" alt="">
+                <p>Price: $10</p>
+            </div>
+            </div>
+            <div class="dinner">
+            <h2 id="dinner">Dinner</h2>
+            <img class="img-bg" src="" alt="">
+            <div class="food-content">
+                <h2>Grilled Chicken</h2>
+                <img class="img-content" src="../img/grilled-chicken.jpeg" alt="">
+                <p>Price: $13</p>
+            </div>
+            </div>
+            <div class="drinks">
+            <h2 id="dinner">Drinks</h2>
+            <img class="img-bg" src="../img/" alt="">
+            <div class="food-content">
+                <h2>Indigo Warrior</h2>
+                <img class="img-content" src="../img/indigo-warrior.jpeg" alt="">
+            </div>
+            <div class="food-content">
+                <h2>Acai Smoothie</h2>
+                <img class="img-content" src="../img/acai-smoothie.jpeg" alt="">
+            </div>
+            <div class="food-content">
+                <h2>Watermelon Shake</h2>
+                <img class="img-content" src="../img/watermelon-shake.jpeg" alt="">
+            </div>
+            </div>
+        </div>
     </main>
 </body>
 </html>
