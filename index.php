98d8d3533d0561d26184a3c5ceed5c38302e4bbd
Katies Truong <31566298+KatiesTruong@users.noreply.github.com>
2018-02-08 14:07:56 +1100

    Update home page

--- index.php
+++ index.php
@@ -9,40 +9,31 @@
 </head>
 <body>
     <header>
-        <!--logo and branding has been placed in nav-->
-    </header>
-    <!--Note to self: will update menu image soon and add delivery services afterwards. Will separate coffee and tea shop when A3 comes along to products1.php and products2.php-->
-    <nav>
-        <div class="logo-panel">
-            <div class="navbar">
-                <span class="nav-logo"><img src="../img/kaffe-logo-nav.png" alt="The KAFFÉ logo is displayed with the navigation bar"></span>
+        <nav id="main-nav">
+            <div id="logo-panel">
+                <span id="nav-logo"><img src="../img/kaffe-logo-nav.png" alt="The KAFFÉ logo is displayed with the navigation bar"></span>
 	                <ul>
 	                    <li><a href="index.php">Home</a></li>
-                            <div class="dropdown">
-                                 <button class="dropbtn">
-                                    <li>Shop<i class="fa fa-sort-desc"></i></li>
-                                 </button>
-                                 <div class="dropdown-content">
-                                     <a href="products.php">Coffee</a>
-                                     <a href="products.php">Tea</a>
-                                 </div>
-                            </div>
-                            <li><a href="login.php">Login</a></li>
-	                        <li><a href="service.php">Bookings</a></li>
-	                        <li><a href="#">Menu</a></li>
-                            <li><a href="service2.php">Delivery</a></li>
-                            <li><a href="#">Contact</a></li>
+                        <li><a href="products.php">Shop<i class="fa fa-sort-desc"></i></a>
+                            <ul>
+                                <li><a href="products.php">Coffee</a></li>
+                                <li><a href="products.php">Tea</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="login.php">Login</a></li>
+	                    <li><a href="service.php">Bookings</a></li>
+	                    <li><a href="#">Menu</a></li>
+                        <li><a href="service2.php">Delivery</a></li>
+                        <li><a href="#">Contact</a></li>
                     </ul>
-                </div>
-                <div class="main-logo">
-                    <img src="../img/kaffe-logo-revamped.png" alt="The KAFFÉ logo">
-                </div>
+                    <img id="main-logo" src="../img/kaffe-logo-revamped.png" alt="The KAFFÉ logo">
             </div>
-        </div>
-    </nav>
+        </nav>
+    </header>
+    <!--Note to self: will update menu image soon and add delivery services afterwards. Will separate coffee and tea shop when A3 comes along to products1.php and products2.php-->
     <main>
         <h1>Welcome to KAFFÉ</h1>
-        <div class="info-paras">
+        <div id="main-info">
             <p>KAFFÉ is a specialty coffee shop serving the best urban dishes along the Boulevarde. We roast the highest quality beans ranging from Columbia, Africa and eco-friendly coffee.</p>
             <p>We take special care of our coffee beans enriching the darkest aromas served with the palette to your warming dishes.</p>
             <p>Our purpose is to entrust meaning to our customer's love for coffee artistry. We value the environment as much as you do, as one of our biggest trademark
