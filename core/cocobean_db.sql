870598bac146860f78547ba3109274903a4e321b
Katies Truong <katies.truong@gmail.com>
2018-09-01 03:14:49 +1000

    Updated database

--- core/cocobean_db.sql
+++ core/cocobean_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 24, 2018 at 04:51 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.2
+-- Host: localhost
+-- Generation Time: Sep 01, 2018 at 03:13 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cocobean`
+-- Database: `cocobean_db`
 --
 
 -- --------------------------------------------------------
@@ -68,11 +68,11 @@ INSERT INTO `products` (`id`, `title`, `price`, `categories`, `image`, `descript
 (2, 'Columbia', 15.00, '1', '/cocobean/img/products/columbia.png', 'Residing from Columbia, this special blend is infused with a citrus-like flavour accompanied with a fruity bean oil', '500g, 1kg', NULL),
 (3, 'Africano', 14.00, '1', '/cocobean/img/products/africano.png', 'Wet pressed from Uganda, these beans consist of a darker bite and holds a richer aroma satisfying many strong coffee lovers.', '500g, 1kg', NULL),
 (4, 'Fairtrade', 15.00, '1', '/cocobean/img/products/fairtrade.png', 'Organically sourced from Peru, its ideal flavour is grown in the higher altitudes of Peru creating a softer and gentler body which gives a greater nutty taste in its bean. ', '500g, 1kg', NULL),
-(5, 'English Breakfast', 10.00, '2', '/cocobean/img/products/english-breakfast.png', 'One of the most popular tea blends in the market. Most commonly as British tea, its robust and rich black tea leaves works well with milk and sugar.', NULL, 'Green, White, Black'),
-(6, 'Early Grey', 10.00, '2', '/cocobean/img/products/earl-grey.png', 'Black tea that is enriched with the oils from the flavours of bergamot orange rind.', NULL, 'Green, White, Black'),
-(7, 'Chai Tea', 12.00, '2', '/cocobean/img/chai-tea.png', 'Commonly known as masala chai, its black tea leaves are usually mixed with aromatic Indian spices and herbs.', NULL, 'Green, White, Black'),
+(5, 'English Breakfast', 10.00, '2', '/cocobean.com/img/products/english-breakfast.png', 'One of the most popular tea blends in the market. Most commonly as British tea, its robust and rich black tea leaves works well with milk and sugar.', NULL, 'Green, White, Black'),
+(6, 'Earl Grey', 10.00, '2', '/cocobean.com/img/products/earl-grey.png', 'Black tea that is enriched with the oils from the flavours of bergamot orange rind.', NULL, 'Green, White, Black'),
+(7, 'Chai Tea', 12.00, '2', '/cocobean.com/img/products/chai.png', 'Commonly known as masala chai, its black tea leaves are usually mixed with aromatic Indian spices and herbs.', NULL, 'Green, White, Black'),
 (8, 'Oolong', 12.00, '2', '/cocobean/img/oolong.png', 'Traditional Chinese tea that is oxidised from the sun\'s heat. It is commonly attributed for giving life-long health benefits.', NULL, 'Green, White, Black'),
-(9, 'Darjeeling', 14.00, '2', '/cocobean/img/darjeeling.png', 'Darjeeling tea usually resides in the northern region of India. Famously known as \"The Champagne Tea\", it consists of a combination of green, white and black tea leaves giving a lighter exotic taste.', NULL, 'Green, White, Black');
+(9, 'Darjeeling', 14.00, '2', '/cocobean.com/img/products/darjeeling.png', 'Darjeeling tea usually resides in the northern region of India. Famously known as \"The Champagne Tea\", it consists of a combination of green, white and black tea leaves giving a lighter exotic taste.', NULL, 'Green, White, Black');
 
 --
 -- Indexes for dumped tables
